d38f664d4630d5ecbf9ec8a9076f6c7e3c811087
James Digrazia <jtd2401@rit.edu>
2018-08-08 13:40:17 -0400

    Setup NodeJS with Create React App

---
+++ client/src/components/header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './header.css'
+
+class Header extends Component {
+    render() {
+        return( 
+        <header>
+            <h1>Paint Guage</h1>
+            <h2>WIP</h2>
+        </header>
+        )
+    }
+}
+
+export default Header;
