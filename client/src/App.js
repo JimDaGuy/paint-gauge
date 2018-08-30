b74d2ca9465466e76b6f699821a7fea5dace1ef1
James Digrazia <jtd2401@rit.edu>
2018-08-30 19:51:38 -0400

    Adds visual rating

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
   state = {
     imageSrc: '',
     artName: '',
-    id: ''
+    id: '',
+    locked: true,
   };
 
   componentDidMount() {
@@ -25,28 +26,41 @@ class App extends Component {
 
   callApi = async () => {
     const response = await fetch('/api/getRandomPainting');
-    const body = await response.json();
 
-    if (response.status !== 200) throw Error(body.message);
+    const body = await response.json()
+      .then(this.ratingComponent.resetStars())
+      .then(this.state.locked = false);
+
+    if (response.status !== 200)
+      throw Error(body.message);
 
     return body;
   };
 
+  //Send a rating for the current painting and grab a new painting
   sendRating = async (e) => {
-    //console.dir(e.target.parentElement.nodeName)
+    //Don't send a rating if the component is locked
+    if (this.state.locked)
+      return;
 
+    //Return from the function if FontAwesome is a little weird and the parent element is missing
     if (e.target.parentElement.nodeName !== 'svg')
       return;
 
-    console.dir(e.target.parentElement)
-
+    //Weird way to get value attribute because I'm using FontAwesome icons
     var rating = e.target.parentElement.attributes.value.value;
     var id = this.state.id;
     var artName = this.state.artName;
 
-    //console.log(`Set rating to: ${rating}, id:${id}, name:${artName}`);
+    //Highlight stars in the rating component
+    this.ratingComponent.highlightStars(rating);
 
-    //const post = 
+    //Lock the rating component to prevent sending ratings before a new image has loaded
+    this.setState({
+      locked: true
+    });
+
+    //Send a POST to the API with the rating, id, and name of the art
     await fetch('/api/sendRating', {
       method: 'POST',
       headers: {
@@ -60,32 +74,33 @@ class App extends Component {
       })
     });
 
-    //post.res
-
+    //Get a new painting to display after sending out a rating
     this.getNewPainting();
   }
 
   getNewPainting = () => {
     this.callApi()
+      //Set the state with results from the API and unlock the rating component
       .then(res => this.setState({
         imageSrc: res.primaryimageurl + '?width=450',
         artName: res.title,
-        id: res.id
+        id: res.id,
+        locked: false
       }))
       .catch(err => {
         throw err;
       });
   }
 
-render() {
-  return (
-    <div className="App">
-      <Header />
-      <PaintCarousel imageSrc={this.state.imageSrc} artName={this.state.artName} />
-      <Ratings artName={this.state.artName} sendRating={this.sendRating} />
-    </div>
-  );
-}
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <PaintCarousel locked={this.state.locked} imageSrc={this.state.imageSrc} artName={this.state.artName} />
+        <Ratings ref={instance => { this.ratingComponent = instance; }} artName={this.state.artName} sendRating={this.sendRating} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- client/src/components/ratings.js
+++ client/src/components/ratings.js
@@ -9,21 +9,42 @@ class Ratings extends Component {
         super(props);
         this.artName = props.artName;
         this.sendRating = props.sendRating.bind(this);
+        this.locked = props.locked;
     }
 
     getValue = () => {
         return this.value;
     }
 
+    highlightStars = (value) => {
+        var stars = document.getElementsByClassName('star');
+
+        Array.prototype.forEach.call(stars, (star) => {
+            var starValue = star.attributes.value.value;
+
+            if (value >= starValue) {
+                star.style.color = "yellow";
+            }
+        });
+    }
+
+    resetStars = () => {
+        var stars = document.getElementsByClassName('star');
+
+        Array.prototype.forEach.call(stars, (star) => {
+            star.style.color = "black";
+        });
+    }
+
     render() {
         return (
             <div className="ratingBar">
                 <h2 className="ratingH1">{this.props.artName}</h2>
-                <FontAwesomeIcon icon={faStar} value="1" onClick={ e => this.props.sendRating(e)}/>
-                <FontAwesomeIcon icon={faStar} value="2" onClick={ e => this.props.sendRating(e)}/>
-                <FontAwesomeIcon icon={faStar} value="3" onClick={ e => this.props.sendRating(e)}/>
-                <FontAwesomeIcon icon={faStar} value="4" onClick={ e => this.props.sendRating(e)}/>
-                <FontAwesomeIcon icon={faStar} value="5" onClick={ e => this.props.sendRating(e)}/>
+                <FontAwesomeIcon className="star" icon={faStar} value="1" onClick={e => this.props.sendRating(e)} />
+                <FontAwesomeIcon className="star" icon={faStar} value="2" onClick={e => this.props.sendRating(e)} />
+                <FontAwesomeIcon className="star" icon={faStar} value="3" onClick={e => this.props.sendRating(e)} />
+                <FontAwesomeIcon className="star" icon={faStar} value="4" onClick={e => this.props.sendRating(e)} />
+                <FontAwesomeIcon className="star" icon={faStar} value="5" onClick={e => this.props.sendRating(e)} />
             </div>
         )
     }
@@ -31,7 +52,8 @@ class Ratings extends Component {
 
 Ratings.propTypes = {
     artName: PropTypes.string,
-    sendRating: PropTypes.func
+    sendRating: PropTypes.func,
+    locked: PropTypes.bool
 };
 
 export default Ratings;
