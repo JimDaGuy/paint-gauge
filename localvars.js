73cb104635a61d63ab32a155e9645a960b53cbe8
James DiGrazia <jtd2401@rit.edu>
2018-09-08 14:01:02 -0400

    Delete localvars.js

--- localvars.js
+++
@@ -1,9 +0,0 @@
-const HARVARD_KEY = 'bccef2b0-a64a-11e8-9063-ff0e89af0957';
-
-const MYSQL_CREDS = {
-  hostname: 'localhost',
-  username: 'root',
-  password: 'localroot',
-};
-
-module.exports = { HARVARD_KEY, MYSQL_CREDS };
